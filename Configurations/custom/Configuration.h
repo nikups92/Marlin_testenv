188a2a792411d912354eedf53af5275403a972a8
nikups92 <antonino.spurio@outlook.com>
2022-07-22 15:48:44 +0200

    changed configuration files in Marlin folder

--- Configurations/custom/Configuration.h
+++ Configurations/custom/Configuration.h
@@ -143,7 +143,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Aquila"
+#define CUSTOM_MACHINE_NAME "Aquila S2"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
